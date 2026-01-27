3f99972e08e9eccb651ae594d0aebdf549bf0081
yw4z <ywsyildiz@gmail.com>
2026-01-27 13:34:03 +0300

    match background color of BBL > Calibration tab > Sidebar

--- src/slic3r/GUI/CalibrationPanel.cpp
+++ src/slic3r/GUI/CalibrationPanel.cpp
@@ -475,7 +475,7 @@ void CalibrationPanel::init_tabpanel() {
 
     m_tabpanel = new Tabbook(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, sizer_side_tools, wxNB_LEFT | wxTAB_TRAVERSAL | wxNB_NOPAGETHEME);
     m_side_tools->set_table_panel(m_tabpanel);
-    m_tabpanel->SetBackgroundColour(*wxWHITE);
+    m_tabpanel->SetBackgroundColour(wxColour("#FEFFFF")); // ORCA match sidebar background color
 
     m_cali_panels[0] = new PressureAdvanceWizard(m_tabpanel);
     m_cali_panels[1] = new FlowRateWizard(m_tabpanel);
