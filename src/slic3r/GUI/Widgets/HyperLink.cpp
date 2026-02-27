e97823c9968d4744b2a15dd5b3e4f05d0a6e8121
yw4z <ywsyildiz@gmail.com>
2026-02-27 22:25:34 +0300

    init

--- src/slic3r/GUI/Widgets/HyperLink.cpp
+++ src/slic3r/GUI/Widgets/HyperLink.cpp
@@ -4,38 +4,49 @@
 namespace Slic3r { namespace GUI {
 
 HyperLink::HyperLink(wxWindow* parent, const wxString& label, const wxString& url, long style)
-    : wxStaticText(parent, wxID_ANY, label)
+    : wxStaticText(parent, wxID_ANY, label, wxDefaultPosition, wxDefaultSize, style | wxUSE_MARKUP)
     , m_url(url)
     , m_normalColor(wxColour("#009687")) // used slightly different color otherwise automatically uses ColorForDark that not visible enough
     , m_hoverColor(wxColour("#26A69A"))
 {
     SetForegroundColour(m_normalColor);
     HyperLink::SetFont(Label::Head_14);
-    SetCursor(wxCursor(wxCURSOR_HAND));
+
+    //SetCursor(wxCursor(wxCURSOR_HAND)); // drectly using on class sets curson on parent permanently on linux
 
     if (!m_url.IsEmpty())
         SetToolTip(m_url);
 
     Bind(wxEVT_LEFT_DOWN, ([this](wxMouseEvent& e) {
-             if (!m_url.IsEmpty())
-                 wxLaunchDefaultBrowser(m_url);
-         }));
+        if (!m_url.IsEmpty())
+            wxLaunchDefaultBrowser(m_url);
+    }));
 
     Bind(wxEVT_ENTER_WINDOW, ([this](wxMouseEvent& e) {
-             SetForegroundColour(m_hoverColor);
-             Refresh();
-         }));
+        SetCursor(wxCursor(wxCURSOR_HAND));
+        SetForegroundColour(m_hoverColor);
+        Refresh();
+    }));
+
     Bind(wxEVT_LEAVE_WINDOW, ([this](wxMouseEvent& e) {
-             SetForegroundColour(m_normalColor);
-             Refresh();
-         }));
+        SetCursor(wxNullCursor); // revert
+        SetForegroundColour(m_normalColor);
+        Refresh();
+    }));
 }
 
 bool HyperLink::SetFont(const wxFont& font)
 { // ensure it stays underlined
-    wxFont f = font;
-    f.SetUnderlined(true);
-    return wxStaticText::SetFont(f);
+    #ifdef __WXMAC__
+        // SetUnderlined not works on macOS. set underline via markup
+        bool result = wxStaticText::SetFont(font);
+        SetLabelMarkup("<u>" + GetLabelText() + "</u>");
+        return result;
+    #else
+        wxFont f = font;
+        f.SetUnderlined(true);
+        return wxStaticText::SetFont(f);
+    #endif
 }
 
 void HyperLink::SetURL(const wxString& url)
