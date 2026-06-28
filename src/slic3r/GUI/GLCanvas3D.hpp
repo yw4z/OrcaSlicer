f415e4a0cb553c6d0782151ccbcaab93d411353e
yw4z <ywsyildiz@gmail.com>
2026-06-28 16:26:55 +0300

    init

--- src/slic3r/GUI/GLCanvas3D.hpp
+++ src/slic3r/GUI/GLCanvas3D.hpp
@@ -1256,6 +1256,7 @@ private:
     void _render_platelist(const Transform3d& view_matrix, const Transform3d& projection_matrix, bool bottom, bool only_current, bool only_body = false, int hover_id = -1, bool render_cali = false, bool show_grid = true);
     //BBS: add outline drawing logic
     void _render_objects(GLVolumeCollection::ERenderType type, bool with_outline = true);
+    void _render_wireframe_overlay();
     //BBS: GUI refactor: add canvas size as parameters
     void _render_gcode(int canvas_width, int canvas_height);
     //BBS: render a plane for assemble
